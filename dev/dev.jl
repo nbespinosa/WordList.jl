186792fd769104cf051992134de0357165fcf184
nbespinosa <britoespinosanayeris@gmail.com>
2023-08-22 19:41:15 -0400

    add: WordList

--- dev/dev.jl
+++ dev/dev.jl
@@ -1,49 +1,88 @@
 
 using WordList
 
-let
-    fn = "/home/nayeris/.julia/dev/WordList/store/blablo.txt"
-    words = load_words(fn)
-    println.(words)
-    typeof(words)
 
-    println("for (version 2)")
-    for wordindex in eachindex(words)
-        println(wordindex, ": ", words[wordindex]) 
-     end
-    
-     for word in words
-        esConA = startswith(word, "a") 
-        if esConA
-            println(word)
+    fn = "/home/nayeris/.julia/dev/WordList/store/listado.txt"
+    words = load_words(fn)    
+    println.(words) 
+    typeof(words)  
+
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 1st condition: large
+
+function large_words(palabras, len)
+    yes_words =[]
+    for word in palabras
+        if length(word) == len
+            push!(yes_words, word)
         end
     end
-
-    function building_words(letters, words)
-    build_words = []
-        for word in words
-        available_letters = copy(letters)
-        is_build = true
-            if is_build
-                push!(build_words, word)
-            end
-        end
-        letters = ['b', 'o', 'y'] 
-        build_words = building_words(letters, words) # return built_words, letters = ['b', 'y', 'o']
-        println(build_words)
-    end
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    function large_words(t, len)
-    yes_words = []
-        for word in words
-            if length(word) == len
-                push!(yes_words, word)
-            end
-        end
-        return yes_words
-    end
-    t = words
-    large_words(words, 3)
+    println(yes_words)
+    return yes_words
 end
+   
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Descomposicion 
+
+function Diccionary(palabras)
+    d = Dict()
+    for word in palabras
+        d[word] = []
+        for letters in word
+            push!(d[word], letters)
+        end
+    end
+    return d
+end
+
+function solve(letters, len, palabras)
+    filtradas = large_words(palabras, len)
+    d = Diccionary(filtradas)
+    verf = []
+    n=length(letters)
+    i=1
+    while i<=n
+        push!(verf,0)
+        i += 1
+    end 
+    solution = []
+    for words in filtradas
+        v = true
+        for letras in d[words]
+            i = 1 
+            k = false
+            while i <= n
+                if letters[i] == letras && verf[i]==0
+                    verf[i] += 1
+                    k=true
+                    break
+                end
+                i+=1
+            end
+            if k==false
+                v=false
+                break
+            end
+        end
+        if v
+            push!(solution,words)
+        end
+        i=1
+        while i<=n     
+            verf[i]=0
+            i+=1
+        end
+    end
+    return solution
+end
+
+l = ['c','u','c','o','e','u','a','v','m','l','e','ñ']
+
+println(solve(l,6,words))   
+
+
+
+
+
